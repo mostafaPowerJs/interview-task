a8b44e39d35fddfba52882ca09bdf02ebc966bf9
seif97-wecancity <saif.elfagal@wecancity.org>
2022-12-10 14:12:44 -0800

    seif4

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
+
 export class NavBarComponent {
+  switch:boolean = true;
+ 
+  ngOnInit(): void {
+    const str = window.location.href;
+const seif = str.split(/[\s/]+/)
+const last = seif[seif.length - 1]
+if( last == 'orders'){
+this.switch = false
+}  
+  }
+
   itemsNav = [
     {
       nameItem : 'ClickTik',
@@ -16,4 +28,8 @@ export class NavBarComponent {
       img : null
     }
   ]
+
+  ngOnDestroy() {
+    this.switch = true
+  }
 }
