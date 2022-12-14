0db7ed1b4a2c6e89450e12491ecad8c39732d15b
mostafa hendy <mostafahendy34@gmail.com>
2022-12-14 21:07:25 +0200

    mos

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  itemsFooter = [
+    {
+      nameItem : 'ClickTik',
+      img : '01.png'
+    },
+    {
+      nameItem : '@ 2022 ClickTik',
+      img : null
+    }
+  ]
 }
